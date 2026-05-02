47ba4c93ea56c0807f0d20397ffe842ba7b422be
340SAMI <aminulislamsami1@gmail.com>
2026-05-02 11:22:06 +0600

    first commit

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mainlayout = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default mainlayout;
---
+++ src/app/(main)/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const main = () => {
+    return (
+        <div>
+            <h3>lghdglfdgh</h3>
+            <h1>wadsdaifsdfj,zdch</h1>
+        </div>
+    );
+};
+
+export default main;
